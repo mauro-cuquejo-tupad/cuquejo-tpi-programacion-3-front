dff5ce232eb27eb4d3a7632b5edea78c4c71a1df
Mauro Cuquejo <mauro.cuquejo@tupad.utn.edu.ar>
2026-06-26 05:48:52 -0300

    Se agregan estilos y funcionalidad filtrado orders

--- src/pages/client/orders/orders.ts
+++ src/pages/client/orders/orders.ts
@@ -5,15 +5,38 @@ import { actualizarContadorCarrito } from "../../store/cart/cart";
 import type { Pedido } from "../../../types/pedido";
 
 let contenedorPedidos: HTMLElement | null = document.querySelector<HTMLElement>("#contenedor-pedidos");
+let selectPedidos: HTMLElement | null = document.querySelector<HTMLElement>("#estados-pedidos");
 
-const renderizarPedidos = (): void => {
+const cargarOpcionesPedidos = (): void => {
+    if (!selectPedidos) return;
+
+    const estadosUnicos = ["TODOS", ...new Set(getPedidos.map(p => p.estado.toUpperCase()))];
+
+    selectPedidos.innerHTML = "";
+    estadosUnicos.forEach(estado => {
+        const option = document.createElement("option");
+        option.value = estado;
+        option.textContent = estado;
+        selectPedidos.appendChild(option);
+    })
+};
+
+const renderizarPedidos = (estadoFiltro: string = "TODOS"): void => {
     if (!contenedorPedidos) return;
 
-    if (getPedidos.length == 0) {
-        contenedorPedidos.innerHTML = "No hay pedidos";
+
+    const pedidosFiltrados = estadoFiltro === "TODOS"
+        ? getPedidos
+        : getPedidos.filter(p => p.estado.toUpperCase() === estadoFiltro);
+
+    if (pedidosFiltrados.length == 0) {
+        contenedorPedidos.innerHTML = "No hay pedidos registrados.</p>";
         return;
     };
-    getPedidos.forEach((pedido: Pedido) => {
+
+    contenedorPedidos.innerHTML = "";
+
+    pedidosFiltrados.forEach((pedido: Pedido) => {
         contenedorPedidos.appendChild(crearItemPedido(pedido));
     })
 
@@ -28,7 +51,7 @@ const crearItemPedido = (pedido: Pedido): HTMLDivElement => {
     fechaPedido.textContent = "📅 - " + convertirFecha(pedido.fecha);
 
     const nombresProductos = pedido.detalles.map(d => d.producto.nombre);
-    console.log(nombresProductos);
+
     const detallesPedidos: HTMLUListElement = document.createElement("ul");
     nombresProductos.forEach(nombre => {
         const elementoPedido = document.createElement("li");
@@ -40,10 +63,12 @@ const crearItemPedido = (pedido: Pedido): HTMLDivElement => {
     spanCantidadDetalles.textContent = (nombresProductos != null ? nombresProductos.length.toString() : "0") + " producto(s)";
 
     const importeTotal: HTMLSpanElement = document.createElement("span");
-    importeTotal.textContent = pedido.total.toFixed(2);
+    importeTotal.textContent = ` Total: $${pedido.total.toFixed(2)}`;
 
     const estadoPedido: HTMLSpanElement = document.createElement("span");
     estadoPedido.textContent = pedido.estado.toUpperCase();
+
+    estadoPedido.classList.add("estado-badge", pedido.estado.toLocaleLowerCase());
 
     itemPedido.appendChild(fechaPedido);
     itemPedido.appendChild(estadoPedido);
@@ -64,8 +89,14 @@ const convertirFecha = (fecha: string): String => {
     }).format(fechaDate);
 };
 
+selectPedidos?.addEventListener("change", (e) => {
+    const target = e.target as HTMLSelectElement;
+    renderizarPedidos(target.value);
+});
+
 
 agregarLogout();
 guardRoutes();
+cargarOpcionesPedidos();
 renderizarPedidos();
 actualizarContadorCarrito();
