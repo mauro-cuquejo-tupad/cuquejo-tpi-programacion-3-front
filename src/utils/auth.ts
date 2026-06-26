4ed6446bfa24fbf406a60162f3a4401ebd909803
Mauro Cuquejo <mauro.cuquejo@tupad.utn.edu.ar>
2026-06-25 23:46:11 -0300

    Optimizacion validaciones paths navegacion

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,7 +2,7 @@
 import type { IUser } from "../types/IUser";
 import { getUSer, removeStoreFilters, removeUser } from "./localStorage";
 import { navigate } from "./navigate";
-import { CART_PAGE, HOME_ADMIN, HOME_CLIENT, HOME_STORE, INDEX_PAGE, LOGIN_PAGE, PRODUCT_DETAIL, REGISTRO_PAGE, ROOT_PAGE, VALID_PAGES } from "./routes"
+import { VALID_PAGES, VALID_ADMIN_PAGES, VALID_USER_PAGES, HOME_STORE, LOGIN_PAGE, REGISTRO_PAGE, HOME_ADMIN } from "./routes"
 
 export const logout = () => {
     removeStoreFilters();
@@ -14,18 +14,8 @@ export const guardRoutes = () => {
     const usuario: string | null = getUSer();
     const pagina: string = window.location.pathname;
 
-    if (!VALID_PAGES.has(pagina)) {
-        navigate(HOME_STORE);
-        return;
-    }
-
-    if (pagina === ROOT_PAGE || pagina === INDEX_PAGE) {
-        navigate(LOGIN_PAGE);
-        return;
-    }
-
     if (!usuario) {
-        if (!(pagina === LOGIN_PAGE || pagina === REGISTRO_PAGE)) {
+        if (!VALID_PAGES.has(pagina)) {
             alert("No hay un usuario logueado. Será redirigido a la página de login");
             navigate(LOGIN_PAGE);
         }
@@ -41,22 +31,14 @@ export const guardRoutes = () => {
             return;
         }
 
-        if (usuarioParseado.role === "USUARIO" && (pagina === HOME_ADMIN ||
-            pagina === LOGIN_PAGE ||
-            pagina === REGISTRO_PAGE ||
-            pagina === ROOT_PAGE)) {
+        if (usuarioParseado.role === "USUARIO" && !VALID_USER_PAGES.has(pagina)) {
             alertaRedireccion(pagina);
 
             navigate(HOME_STORE);
             return;
         }
 
-        if (usuarioParseado.role === "ADMIN" && (pagina === HOME_STORE ||
-            pagina === LOGIN_PAGE ||
-            pagina === REGISTRO_PAGE ||
-            pagina === HOME_CLIENT ||
-            pagina === PRODUCT_DETAIL ||
-            pagina === CART_PAGE)) {
+        if (usuarioParseado.role === "ADMIN" && !VALID_ADMIN_PAGES.has(pagina)) {
             alertaRedireccion(pagina);
             navigate(HOME_ADMIN);
             return;
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -4,18 +4,23 @@ export const HOME_CLIENT = "/src/pages/client/client.html";
 export const LOGIN_PAGE = "/src/pages/auth/login/login.html";
 export const REGISTRO_PAGE = "/src/pages/auth/registro/registro.html";
 export const CART_PAGE = "/src/pages/store/cart/cart.html";
+export const ORDER_PAGE = "/src/pages/client/orders/orders.html";
 export const PRODUCT_DETAIL = "/src/pages/store/productDetail/productDetail.html";
 export const ROOT_PAGE = "/";
 export const INDEX_PAGE = "/index.html";
 
-export const VALID_PAGES = new Set<string>([
-    ROOT_PAGE,
-    INDEX_PAGE,
-    HOME_STORE,
-    HOME_ADMIN,
-    HOME_CLIENT,
+export const VALID_PAGES = new Set<String>([
     LOGIN_PAGE,
     REGISTRO_PAGE,
+]);
+export const VALID_USER_PAGES = new Set<string>([
+    HOME_STORE,
+    HOME_CLIENT,
     CART_PAGE,
-    PRODUCT_DETAIL
+    PRODUCT_DETAIL,
+    ORDER_PAGE
+]);
+
+export const VALID_ADMIN_PAGES = new Set<string>([
+    HOME_ADMIN
 ]);
