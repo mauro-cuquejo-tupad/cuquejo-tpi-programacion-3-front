4124fdefa7810410f8126fd2cd1d5a0e8296277d
Mauro Cuquejo <mauro.cuquejo@tupad.utn.edu.ar>
2026-06-28 16:21:21 -0300

    Se agrega vista categorias

---
+++ src/pages/admin/categories/categories.ts
@@ -0,0 +1,37 @@
+import { guardRoutes } from "../../../utils/auth";
+import { agregarLogout } from "../../../utils/helpersDom";
+import { getCategorias } from "../../../utils/fetch";
+import type { ICategoria } from "../../../types/categoria";
+
+
+const tbodyCategorias = document.querySelector<HTMLTableSectionElement>("#tabla-crud-categorias");
+
+const renderizarTablaCategorias = (): void => {
+    if (!tbodyCategorias) return;
+    tbodyCategorias.innerHTML = "";
+
+    getCategorias.forEach((cat: ICategoria) => {
+        if (cat.eliminado) return;
+        const fila: HTMLTableRowElement = document.createElement("tr");
+
+        const id: HTMLTableCellElement = document.createElement("td");
+        id.textContent = `#${cat.id}`;
+
+        const nombre: HTMLTableCellElement = document.createElement("td");
+        nombre.textContent = cat.nombre;
+
+        const descripcion: HTMLTableCellElement = document.createElement("td");
+        descripcion.textContent = cat.descripcion;
+
+        fila.appendChild(id);
+        fila.appendChild(nombre);
+        fila.appendChild(descripcion);
+
+        tbodyCategorias.appendChild(fila);
+    });
+
+};
+
+agregarLogout();
+guardRoutes();
+renderizarTablaCategorias();
