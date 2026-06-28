68ced7f19ce400156e861964be6a842b69ccc0ac
Mauro Cuquejo <mauro.cuquejo@tupad.utn.edu.ar>
2026-06-28 15:54:12 -0300

    Reestructuracion pagina y agregados botones

--- src/pages/admin/adminHome/adminHome.ts
+++ src/pages/admin/adminHome/adminHome.ts
@@ -1,6 +1,12 @@
 import { guardRoutes } from "../../../utils/auth";
 import { getCategorias, getPedidos, getProductos } from "../../../utils/fetch";
 import { agregarLogout } from "../../../utils/helpersDom";
+import { navigate } from "../../../utils/navigate";
+import { ADMIN_CATEGORIES, ADMIN_ORDERS, ADMIN_PRODUCTS } from "../../../utils/routes";
+
+let botonGestionarCategorias: HTMLButtonElement | null = document.querySelector<HTMLButtonElement>("#gestionar-categorias");
+let botonGestionarProductos: HTMLButtonElement | null = document.querySelector<HTMLButtonElement>("#gestionar-productos");
+let botonGestionarPedidos: HTMLButtonElement | null = document.querySelector<HTMLButtonElement>("#gestionar-pedidos");
 
 let totalCategorias: HTMLParagraphElement | null = document.querySelector<HTMLParagraphElement>("#total-categorias");
 let totalProductos: HTMLParagraphElement | null = document.querySelector<HTMLParagraphElement>("#total-productos");
@@ -26,6 +32,11 @@ const calcularProductosActivos = (): void => {
     if (!totalProductosActivos) return;
     totalProductosActivos.textContent = getProductos != null ? getProductos.filter(p => p.disponible && !p.eliminado).length.toString() : "0";
 };
+
+botonGestionarCategorias?.addEventListener("click", () => navigate(ADMIN_CATEGORIES));
+botonGestionarPedidos?.addEventListener("click", () => navigate(ADMIN_ORDERS));
+botonGestionarProductos?.addEventListener("click", () => navigate(ADMIN_PRODUCTS));
+
 
 agregarLogout();
 guardRoutes();
