714e53e9a93d87942dd189cfa9defd9641491241
Mauro Cuquejo <mauro.cuquejo@tupad.utn.edu.ar>
2026-06-28 09:34:08 -0300

    Se elimina referencia a registro page de rutas

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -5,7 +5,6 @@ export const ADMIN_CATEGORIES = "/src/pages/admin/categories/categories.html";
 export const ADMIN_PRODUCTS = "/src/pages/admin/products/products.html";
 export const HOME_CLIENT = "/src/pages/client/client.html";
 export const LOGIN_PAGE = "/src/pages/auth/login/login.html";
-export const REGISTRO_PAGE = "/src/pages/auth/registro/registro.html";
 export const CART_PAGE = "/src/pages/store/cart/cart.html";
 export const ORDER_PAGE = "/src/pages/client/orders/orders.html";
 export const PRODUCT_DETAIL = "/src/pages/store/productDetail/productDetail.html";
@@ -13,8 +12,7 @@ export const ROOT_PAGE = "/";
 export const INDEX_PAGE = "/index.html";
 
 export const VALID_PAGES = new Set<String>([
-    LOGIN_PAGE,
-    REGISTRO_PAGE,
+    LOGIN_PAGE
 ]);
 export const VALID_USER_PAGES = new Set<string>([
     HOME_STORE,
