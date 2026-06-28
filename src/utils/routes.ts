9138476ce0821dc7b7c50620693532d8ecc443a9
Mauro Cuquejo <mauro.cuquejo@tupad.utn.edu.ar>
2026-06-28 10:18:28 -0300

    se elimina referencia a client_home

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -3,7 +3,6 @@ export const HOME_ADMIN = "/src/pages/admin/adminHome/adminHome.html";
 export const ADMIN_ORDERS = "/src/pages/admin/orders/orders.html";
 export const ADMIN_CATEGORIES = "/src/pages/admin/categories/categories.html";
 export const ADMIN_PRODUCTS = "/src/pages/admin/products/products.html";
-export const HOME_CLIENT = "/src/pages/client/client.html";
 export const LOGIN_PAGE = "/src/pages/auth/login/login.html";
 export const CART_PAGE = "/src/pages/store/cart/cart.html";
 export const ORDER_PAGE = "/src/pages/client/orders/orders.html";
@@ -16,7 +15,6 @@ export const VALID_PAGES = new Set<String>([
 ]);
 export const VALID_USER_PAGES = new Set<string>([
     HOME_STORE,
-    HOME_CLIENT,
     CART_PAGE,
     PRODUCT_DETAIL,
     ORDER_PAGE
